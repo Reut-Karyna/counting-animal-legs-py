49a5831be167eaceb6e983d51dc1dd525f767a16
Reut Karyna <89328520+Reut-Karyna@users.noreply.github.com>
2023-04-20 03:38:50 +0800

    how-many-animals-legs.py

---
+++ how-many-animals-legs.py
@@ -0,0 +1,14 @@
+cows1 = input("How many cows do you have?: ")
+chickens1 = input("How many chickens do you have?: ")
+pigs1 = input("How many pigs do you have?: ")
+cows = int(cows1)
+chickens = int(chickens1)
+pigs = int(pigs1)
+
+cows_legs = (cows * 4)
+pigs_legs = (pigs * 4)
+chickens_legs = (chickens * 2)
+
+print ("You have {} cows legs in a farm.".format(cows_legs))
+print ("You have {} pighs legs in a farm.".format(pigs_legs))
+print ("You have {} chickens legs in a farm.".format(chickens_legs))
